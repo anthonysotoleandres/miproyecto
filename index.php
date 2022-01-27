15b3f04162464112c25395469530a38f070faaf1
anthonysotoleandres <1012020192@unajma.edu.pe>
2022-01-27 15:39:57 -0500

    new1

--- index.php
+++ index.php
@@ -3,6 +3,8 @@ echo "hola mundo";
 echo "como estan xdddbetfvd ";
 echo "hola mundo";
 echo "como t35rwedf dfestan xdddbetfvd ";
+echo "hola mundrtegtgefo";
+echo "como estan xdddbetfvd ";
 $a =1;
 $b=2;
 echo $a * $b;
